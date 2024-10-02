8ca0c44fe61aab42c81c8e57bd1055cf6711d971
Muhammad Usman <128804456+Usman5540@users.noreply.github.com>
2024-10-02 15:05:31 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const connectToDatabase = require('./utils/DB')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+
 
 // Load environment variables
 dotenv.config();
 
 // Create Express app
 const app = express();
-app.get("/", (req, res) => res.json({ message: "working auditSoft" }));
-
+app.get('/', async (req, res) => {
+  try {
+    await connectToDatabase(); // Ensure the database is connected
+    res.json({ message: 'DB connection successful' });
+  } catch (error) {
+    res.status(500).json({ message: 'DB connection failed' });
+  }
+});
 // Middleware to parse JSON
 app.use(express.json());
+app.use("/",(req,res)=>{
+    res.json({message:"working"})
+})
 // Routes
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
